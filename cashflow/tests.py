61b8839442fd606dfe6059b632dc48e5446d97e8
Nikita Hismatov <hismatovns@gmail.com>
2011-08-12 13:08:25 +0600

    разбросал тесты по разным методам

--- cashflow/tests.py
+++ cashflow/tests.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 from django.test.client import Client as InternetClient
 from cashflow.models import *
 
-class SimpleTest(TestCase):
+class BaseRESTTest(TestCase):
     def setUp(self):
         user = User.objects.create_user('test', 'test', password='test')
         self.user = user
@@ -21,35 +21,52 @@ class SimpleTest(TestCase):
         self.cur = Currency(title='Ya money', code='YANDEX', payment_backend=self.payment_backend)
         self.cur.save()
 
+        self.c = InternetClient()
+
+    def post(self, params):
+        return self.c.post(self.url, params)
+
     def tearDown(self):
+        self.c.logout()
         self.client_user.delete()
         self.user.delete()
         self.payment_backend.delete()
         self.cur.delete()
 
-    def test_listing(self):
+
+class ListingTest(BaseRESTTest):
+    def setUp(self):
+        super(ListingTest, self).setUp()
+        self.url = reverse('currs_list')
+
+    def test_listing_in_model(self):
         listing = Currency.get_listing()
         self.assertTrue(self.cur.code in listing)
 
     def test_list_rest(self):
-        c = InternetClient()
-        url = reverse('currs_list')
-        annon_resp = c.post(url, {})
+        annon_resp = self.post({})
         self.assertEqual(annon_resp.status_code, 403)
 
-        c.login(username='test', password='test')
-        logged_in_resp = c.post(url, {})
+        self.c.login(username='test', password='test')
+        logged_in_resp = self.c.post(self.url, {})
         o = json.loads(logged_in_resp.content)
         self.assertTrue(self.cur.code in o['currs_list'])
 
-    def test_create_payment_rest(self):
-        c = InternetClient()
-        url = reverse('create_payment')
 
-        annon_resp = c.post(url, {})
+class CreatePaymentTest(BaseRESTTest):
+    def setUp(self):
+        super(CreatePaymentTest, self).setUp()
+        self.url = reverse('create_payment')
+
+    def test_create_payment_rest_annon403(self):
+        annon_resp = self.c.post(self.url, {})
         self.assertEqual(annon_resp.status_code, 403)
 
-        c.login(username='test', password='test')
+    def login(self):
+        self.c.login(username='test', password='test')
+
+    def test_create_payment_rest_ok(self):
+        self.login()
         # все четко
         params = {
             'amount': 42.50,
@@ -58,26 +75,28 @@ class SimpleTest(TestCase):
             'success_url': 'http://66.ru/success/',
             'fail_url': 'http://66.ru/fail/',
         }
-        req = c.post(url, params)
+        req = self.post(params)
         result = json.loads(req.content)
         self.assertEqual(result['status'], 'ok')
         self.assertEqual(result['payment_id'], 1)
-        p = Payment.objects.get(pk=1)
+        p = Payment.objects.get()
         self.assertEqual(p.amount, Decimal('42.5'))
         self.assertEqual(p.currency, self.cur)
         self.assertEqual(p.backend, self.payment_backend)
         self.assertEqual(p.client, self.client_user)
 
 
+    def test_create_payment_rest_minimum(self):
+        self.login()
         # все по минимуму
         params = {
             'amount': 42.50,
             'currency_code': self.cur.code,
         }
-        req1 = c.post(url, params)
-        result = json.loads(req1.content)
+        req = self.post(params)
+        result = json.loads(req.content)
         self.assertEqual(result['status'], 'ok')
-        p = Payment.objects.get(pk=2)
+        p = Payment.objects.get()
         self.assertEqual(p.amount, Decimal('42.5'))
         self.assertEqual(p.currency, self.cur)
 
@@ -87,35 +106,43 @@ class SimpleTest(TestCase):
         self.assertEqual(p.success_url, '')
         self.assertEqual(p.fail_url, '')
 
+    def test_create_payment_rest_empty_form(self):
+        self.login()
         # просто плохая форма
         params = {}
-        req2 = c.post(url, params)
-        result = json.loads(req2.content)
+        req = self.post(params)
+        result = json.loads(req.content)
         self.assertEqual(result['status'], 'invalid form')
 
+    def test_create_payment_rest_silly_cur(self):
+        self.login()
         # вариант со странной валютой
         params = {
             'amount': 42.50,
             'currency_code': 'PAPER',
         }
-        req3 = c.post(url, params)
+        req3 = self.post(params)
         result = json.loads(req3.content)
         self.assertEqual(result['status'], 'invalid form')
 
+    def test_create_payment_negative_amount(self):
+        self.login()
         # вариант с отрицательным числом
         params = {
             'amount': -200,
             'currency_code': self.cur.code,
         }
-        req4 = c.post(url, params)
-        result = json.loads(req4.content)
+        req = self.post(params)
+        result = json.loads(req.content)
         self.assertEqual(result['status'], 'invalid form')
 
+    def test_create_payment_rest_str_price(self):
+        self.login()
         # вариант со строкой вместо числа
         params = {
             'amount': 'yahrr!',
             'currency_code': self.cur.code,
         }
-        req5 = c.post(url, params)
+        req5 = self.post(params)
         result = json.loads(req5.content)
         self.assertEqual(result['status'], 'invalid form')
