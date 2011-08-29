7a2ed13e40169b21bbfc25ceda753e3a0644eaed
Nikita Hismatov <hismatovns@gmail.com>
2011-08-29 13:05:53 +0600

    нормалньые колонки в админках

--- cashflow/admin.py
+++ cashflow/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import *
 
+class BackendsAdmin(admin.ModelAdmin):
+    list_display = ('slug',)
 
-admin.site.register(Backend)
+admin.site.register(Backend, BackendsAdmin)
 admin.site.register(Client)
-admin.site.register(ClientBackend)
-admin.site.register(Currency)
-admin.site.register(Payment)
+
+class ClientBackendAdmin(admin.ModelAdmin):
+    list_display = ('client', 'backend',)
+
+admin.site.register(ClientBackend, ClientBackendAdmin)
+
+
+class CurrencyBackend(admin.ModelAdmin):
+    list_display = ('title', 'code', 'backend',)
+admin.site.register(Currency, CurrencyBackend)
+
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ('client', 'amount', 'currency', 'backend', 'comment', 'status',)
+
+admin.site.register(Payment, PaymentsAdmin)
