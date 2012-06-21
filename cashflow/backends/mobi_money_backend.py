6e175ac130f9e7bfb60ca852d34a82747530e19c
apahomov <gorblnu4@gmail.com>
2012-06-21 18:26:34 +0400

    Релизовал wsdl  идентичный shop.wsdl

--- cashflow/backends/mobi_money_backend.py
+++ cashflow/backends/mobi_money_backend.py
@@ -3,7 +3,7 @@ from urlparse import parse_qs
 from django.http import HttpResponse
 from cashflow.models import Payment
 from rpclib.server.django import DjangoApplication
-from rpclib.model.primitive import String, Integer, DateTime
+from rpclib.model.primitive import String, Integer, DateTime, Float, Boolean
 from rpclib.model.complex import  ComplexModel
 from rpclib.service import ServiceBase
 from rpclib.interface.wsdl import Wsdl11
@@ -13,17 +13,9 @@ from rpclib.decorator import rpc
 from django.views.decorators.csrf import csrf_exempt
 
 
-class PaymentContract(ComplexModel):
-    __namespace__ = __name__
-    Sum = Integer
-    Contract = String
-    PayeeRegData = String
-    PaymentDelay = Integer
-
-
 class MobiMoneyService(ServiceBase):
-    @rpc(Integer, DateTime,String, _returns=PaymentContract)
-    def payment_contract(ctx, PaymentID, PaymentTime, UserParams):
+    @rpc(String, DateTime,String, Boolean, _returns=(String, Float, String, Integer), _out_variable_names=('PayeeRegData', 'Sum', 'Contract', 'PaymentDelay'))
+    def PaymentContract(ctx, PaymentID, PaymentTime, UserParams, Demo):
         user_params = parse_qs(UserParams)
         payment_pk = int(user_params['payment_pk'])
         payment = Payment.objects.get(pk = payment_pk)
@@ -37,13 +29,37 @@ class MobiMoneyService(ServiceBase):
         '''%(payment.comment, payment.amount)
         PayeeRegData = "%s"%payment_pk
         PaymentDelay = 0
-        return PaymentContract(Sum = Sum, Contract = Contract, PayeeRegData=PayeeRegData, PaymentDelay=PaymentDelay)
+        return PayeeRegData, Sum, Contract, PaymentDelay
 
-        #    @rpc(Integer, DateTime,String, _returns=Array)
-#    def payment_authorization(ctx, PaymentID, PaymentTime, UserParams):
-#        return []
+    @rpc(String, String, DateTime,Integer,Float,Boolean, _returns=(String, Boolean), _out_variable_names=('ReplyResourse', 'ReplyResourceIsFailure'))
+    def PaymentAuthorization(ctx, PaymentID, PayeeRegData,  PaymentTime, Currency, Sum, IsRepeat):
+        payment_pk = int(PayeeRegData)
+        payment = Payment.objects.get(pk = payment_pk)
+        if Sum == payment.amount:
+            payment.status = Payment.STATUS_SUCCESS
+        payment.save()
+        #TODO:уточнить формат ответа
+        ReplyResourse = '''
+        <?xml version="1.0" encoding="utf-8"?>
+            <contract>
+                <param id="comment" ref="comment" label="Комментарий">%s</param>
+                <param id="sum" ref="sum" label="Сумма">%s</param>
+            </contract>
+        '''%(payment.comment, payment.amount)
+        ReplyResourceIsFailure = False
+        return ReplyResourse, ReplyResourceIsFailure
+
+    @rpc(String, DateTime, Integer,String,String)
+    def PaymentCancellatuon(ctx, PaymentID, PaymentTime,  PaymentResult, PaymentResultStr, PayeeRegData):
+        payment_pk = int(PayeeRegData)
+        payment = Payment.objects.get(pk = payment_pk)
+        payment.status = Payment.STATUS_FAILED
+        payment.status = PaymentResultStr
+        payment.save()
+
+
 mobi_money_service = csrf_exempt(DjangoApplication(Application([MobiMoneyService],
-    __name__,
+   'urn:PaycashShopService',
     interface=Wsdl11(),
     in_protocol=Soap11(),
     out_protocol=Soap11()
